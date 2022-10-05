20d57f65fd7c12e7d37e5ab74093cdffb21f4444
Bryan <chan.bryan13@gmail.com>
2022-10-04 22:28:02 -0700

    Adjusted javascript code to read proper time left

--- countdown.js
+++ countdown.js
@@ -6,32 +6,43 @@ const hoursPerDay = 24;                                         //How many hours
 setInterval(countdown, milisecondsPerSecond);
 
 function countdown() {
-const tripDate = new Date("March 3, 2023 08:15:00");            //Set our trip date
+    const tripDate = new Date("March 3, 2023 08:15:00");            //Set our trip date
 
-let currentTime = new Date();                                   //Set current date
+    let currentTime = new Date();                                   //Set current date
 
-var seconds = 60 - currentTime.getSeconds();                    //Create second countdown
-var minutes = 60 - currentTime.getMinutes();                    //Create minute countdown
-var hours = 23 - currentTime.getHours();                        //Create hour countdown
+    var seconds = 59 - currentTime.getSeconds();                    //Create second countdown
 
-let timeLeft = tripDate.getTime() - currentTime.getTime();      //Calculate timeleft in miliseconds
+    var minutes = 59 - currentTime.getMinutes();                    //Create minute countdown
+    var hours = 23 - currentTime.getHours();                        //Create hour countdown
 
-var totalSeconds = timeLeft / milisecondsPerSecond;
-var totalMinutes = totalSeconds / secondsPerMinute;
-var totalHours = totalMinutes / minutesPerHour;
-var totalDays = totalHours / hoursPerDay;                       //Converted miliseconds to days left
+    if(seconds == 0) {
+        minutes--;
+    }
+    if(minutes == 0) {
+        hours--;
+    }
+    if(hours == 0) {
+        days--;
+    }
 
-var countdownCards = document.getElementsByClassName("time");
-countdownCards[0].querySelector("h2").innerHTML = Math.floor(totalDays);
-countdownCards[1].querySelector("h2").innerHTML = hours;
-countdownCards[2].querySelector("h2").innerHTML = minutes;
-countdownCards[3].querySelector("h2").innerHTML = seconds;
+    let timeLeft = tripDate.getTime() - currentTime.getTime();      //Calculate timeleft in miliseconds
+
+    var totalSeconds = timeLeft / milisecondsPerSecond;
+    var totalMinutes = totalSeconds / secondsPerMinute;
+    var totalHours = totalMinutes / minutesPerHour;
+    var totalDays = totalHours / hoursPerDay;                       //Converted miliseconds to days left
+
+    var countdownCards = document.getElementsByClassName("time");
+    countdownCards[0].querySelector("h2").innerHTML = Math.floor(totalDays);
+    countdownCards[1].querySelector("h2").innerHTML = hours;
+    countdownCards[2].querySelector("h2").innerHTML = minutes;
+    countdownCards[3].querySelector("h2").innerHTML = seconds;
 
 //End countdown when time left (in miliseconds) reaches zero
 if(timeLeft == 0) {
     return;
 }
 
-//Display contents
-//document.getElementById("countdown").innerHTML = Math.floor(totalDays) + " " + hours + " " + minutes + " " + seconds;
+    //Display contents
+    //document.getElementById("countdown").innerHTML = Math.floor(totalDays) + " " + hours + " " + minutes + " " + seconds;
 }
