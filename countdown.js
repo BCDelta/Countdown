d94da7bf541349e5ee2fe21676afeeaeac80795b
Bryan <chan.bryan13@gmail.com>
2022-10-05 20:05:59 -0700

    Reworked javascript to read correct time

--- countdown.js
+++ countdown.js
@@ -9,12 +9,12 @@ function countdown() {
     const tripDate = new Date("March 3, 2023 08:15:00");            //Set our trip date
 
     let currentTime = new Date();                                   //Set current date
-
+    /*
     var seconds = 59 - currentTime.getSeconds();                    //Create second countdown
 
     var minutes = -(currentTime.getMinutes() - tripDate.getMinutes());
     var hours = 23 - (currentTime.getHours() - tripDate.getHours());
-
+    */
     let timeLeft = tripDate.getTime() - currentTime.getTime();      //Calculate timeleft in miliseconds
 
     var totalSeconds = timeLeft / milisecondsPerSecond;
@@ -22,6 +22,18 @@ function countdown() {
     var totalMinutes = totalSeconds / secondsPerMinute;
     var totalHours = totalMinutes / minutesPerHour;
     var totalDays = totalHours / hoursPerDay;                       //Converted miliseconds to days left
+
+    var seconds = 59 - Math.abs(currentTime.getSeconds());
+
+    var minutes =  Math.abs(currentTime.getMinutes() - tripDate.getMinutes());
+    if(currentTime.getMinutes() > tripDate.getMinutes()) {
+        minutes = 59 - (currentTime.getHours() - tripDate.getHours());
+    }
+
+    var hours = currentTime.getHours() - tripDate.getHours();
+    if(currentTime.getHours() >= tripDate.getHours()) {
+        hours = 23 - (currentTime.getHours() - tripDate.getHours());
+    }
 
     var countdownCards = document.getElementsByClassName("time");
     countdownCards[0].querySelector("h2").innerHTML = Math.floor(totalDays);
