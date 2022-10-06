f30db3b69ccd51d2b39207a87a5a4e49dfaa8ee8
Bryan <chan.bryan13@gmail.com>
2022-10-05 21:41:14 -0700

    Final fix to javascript code

--- countdown.js
+++ countdown.js
@@ -5,46 +5,55 @@ const hoursPerDay = 24;                                         //How many hours
 
 setInterval(countdown, milisecondsPerSecond);
 
+var seconds, minutes, hours;
+
 function countdown() {
     const tripDate = new Date("March 3, 2023 08:15:00");            //Set our trip date
 
     let currentTime = new Date();                                   //Set current date
-    /*
-    var seconds = 59 - currentTime.getSeconds();                    //Create second countdown
 
-    var minutes = -(currentTime.getMinutes() - tripDate.getMinutes());
-    var hours = 23 - (currentTime.getHours() - tripDate.getHours());
-    */
     let timeLeft = tripDate.getTime() - currentTime.getTime();      //Calculate timeleft in miliseconds
 
     var totalSeconds = timeLeft / milisecondsPerSecond;
     
     var totalMinutes = totalSeconds / secondsPerMinute;
     var totalHours = totalMinutes / minutesPerHour;
-    var totalDays = totalHours / hoursPerDay;                       //Converted miliseconds to days left
+    var totalDays = (totalHours / hoursPerDay);                       //Converted miliseconds to days left
 
-    var seconds = 59 - Math.abs(currentTime.getSeconds());
+    if(timeLeft > 0) {
+        seconds = 60 - (currentTime.getSeconds() - tripDate.getSeconds()) - 1;
 
-    var minutes =  Math.abs(currentTime.getMinutes() - tripDate.getMinutes());
-    if(currentTime.getMinutes() > tripDate.getMinutes()) {
-        minutes = 59 - (currentTime.getHours() - tripDate.getHours());
+        if(currentTime.getMinutes() > tripDate.getMinutes()) {
+            minutes = 60 - (currentTime.getMinutes() - tripDate.getMinutes());
+        }
+        else {
+            minutes = Math.abs(currentTime.getMinutes() - tripDate.getMinutes()) - 1;
+        }
+
+        if(currentTime.getHours() > tripDate.getHours()) {
+            hours = 24 - Math.abs(currentTime.getHours() - tripDate.getHours());
+        }
+        else {
+            hours = Math.abs(currentTime.getHours() - tripDate.getHours()) - 1;
+        }
+
+        if(seconds < 1) {
+            minutes--;
+        }
+        if(minutes < 1) {
+            hours--;
+            minutes = 59;
+        }
+        if(hours < 1) {
+            totalDays--;
+            hours = 23;
+        }
+        var countdownCards = document.getElementsByClassName("time");
+        countdownCards[0].querySelector("h2").innerHTML = Math.floor(totalDays);
+        countdownCards[1].querySelector("h2").innerHTML = hours;
+        countdownCards[2].querySelector("h2").innerHTML = minutes;
+        countdownCards[3].querySelector("h2").innerHTML = seconds;
     }
-
-    var hours = currentTime.getHours() - tripDate.getHours();
-    if(currentTime.getHours() >= tripDate.getHours()) {
-        hours = 23 - (currentTime.getHours() - tripDate.getHours());
-    }
-
-    var countdownCards = document.getElementsByClassName("time");
-    countdownCards[0].querySelector("h2").innerHTML = Math.floor(totalDays);
-    countdownCards[1].querySelector("h2").innerHTML = hours;
-    countdownCards[2].querySelector("h2").innerHTML = minutes;
-    countdownCards[3].querySelector("h2").innerHTML = seconds;
-
-//End countdown when time left (in miliseconds) reaches zero
-if(timeLeft == 0) {
-    return;
-}
 
     //Display contents
     //document.getElementById("countdown").innerHTML = Math.floor(totalDays) + " " + hours + " " + minutes + " " + seconds;
